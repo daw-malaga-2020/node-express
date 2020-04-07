aefffc873683fc0fdc3921a8f6de22d35dbacef8
elvira <elvirapedregal@hotmail.es>
2020-04-07 19:35:24 +0200

    aclarando apuntes

--- students/elvira_pedregal/mi-primera-web-con-express/server.js
+++ students/elvira_pedregal/mi-primera-web-con-express/server.js
@@ -1,6 +1,6 @@
-const express = require('express') //incluir  dependencia
+const express = require('express') //INCLUIR DEPENDENCIAS
 const port = process.env.PORT || 8080
-const app = express()//crea un servidor // app es instancia de la aplicacion de express
+const app = express()//CREACION DE LA INSTANCIA  // app es instancia de la aplicacion de express
 
 // no podemos acceder a app en otro fichero. dentro del objeto res tenemos la propiedad .app donde podemos acceder a las variables globales o a otros metodos disponibles.
 
@@ -8,33 +8,32 @@ const app = express()//crea un servidor // app es instancia de la aplicacion de 
 
 app.set('app-name','my test app');
 
-//recibe el nombre que le queramos poner a la variable global y el valor que (en este caso es una cadena de texto) puede ser un json, un numero, un booleano, lo que queramos.
+//app.set recibe el nombre que le queramos poner a la variable global y el valor que (en este caso es una cadena de texto) puede ser un json, un numero, un booleano, lo que queramos.
 //como usamos esa variable global dentro del codigo?
 
 
-//esta variable se ejecuta cuando alguien hace una peticion sobre la ruta principal:
-//arranco el servidor
-//mandará una respuesta
+//esta variable se ejecuta cuando alguien hace una peticion sobre la ruta principal de mi sitio con un parametro nombre opcional.
+//se ESCUCHAR las peticiones sobre las rutas y se realizan operaciones segun la ruta
+//DEFINIR una RESPUESTA
+//creacion de una ruta get
 app.get('/:nombre?', (req , res) => {
   if(req.params.nombre){
     res.send(`Hola ${req.params.nombre} esta app se llama ${app.get('app-name')}`)
   } else {
     res.send(`Esta app se llama ${app.get('app-name')}`)
   }
-  //si quiero obtener el valor de la variable global 'app-name'  = app.get(nombre de la variable) como parametro.
-  //en este caso envia como respuesta el nombre de la variable global que hemos definido como un cadena de texto con el nombro de la aplicacion 'my test app' cuando alguien llama a la ruta principal
 })
+//si quiero obtener el valor de la variable global 'app-name'  = app.get(nombre de la variable) como parametro.
+  //en este caso envia como respuesta el nombre de la variable global que hemos definido como un cadena de texto con el nombro de la aplicacion 'my test app' cuando alguien llama a la ruta principal.
 
 
-
-
-//ej solicitar la pagina principal de mi sitio, siendo esta una single page.
+// ej: creacion de una ruta POST se ejecutará si se realiza una peticion de envio de un formulario
 app.post('/', (req , res) => {
   res.send('POST enviado a la pagina principal')
 })
 
 
-// ej, se ejecutará en el envio de un formulario
+//ARRANCO el servidor
 app.listen(port,() => {
   console.log(`servidor activo en http://localhost:${port}!`)
 })
