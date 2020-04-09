8c7855b40d4eb074e266dd5d29dfa9be849b508a
Espe <ego.egarco01@gmail.com>
2020-04-09 18:48:02 +0200

    katas con express

---
+++ students/esperanza_garcia/13.Kata.ConfiguracionServidorNodeDesdeDotenv/server.js
@@ -0,0 +1,18 @@
+const envVars = require('./config')
+const express = require('express')
+
+const app= express()
+const serverPort = process.env.PORT || envVars.SERVER_PORT
+
+app.set('api_version', envVars.STRIPE_API_VERSION)
+
+app.get('/',(req,res)=>{
+  let apiVersion= req.app.get('api_version')
+
+  res.send('La version de mi API es:'+apiVersion)
+})
+
+
+app.listen(serverPort, ()=>{
+  console.info(`Server running in http://localhost:${serverPort}`)
+})
