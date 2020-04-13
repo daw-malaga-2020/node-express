496b20c8803d1ddcb69d78a4f51e7bcb5796bb46
Francisco J. Álvarez <fcojalvarezrodriguez@gmail.com>
2020-04-13 17:38:49 +0200

    Error resuelto en kata DNI

--- students/francisco_javier_alvarez/RecogerParametroYProcesarlo/server.js
+++ students/francisco_javier_alvarez/RecogerParametroYProcesarlo/server.js
@@ -11,12 +11,15 @@ function getLetterDNI(dni) {
 }
 
 app.get('/calcular-dni/:dni', (req, res) => {
-    if (req.params.dni != isNaN) {
-        res.end(`La letra que corresponde al DNI insertardo es la ${getLetterDNI(req.params.dni)} y su DNI completo es ${req.params.dni}${getLetterDNI(req.params.dni)}`)
-    } else {
-        console.log('hola')
+    console.log()
+    if (isNaN(req.params.dni) || req.params.dni.length !== 8) {
+        res.end('El dni introducido no es correcto, por favor introduzca uno en el final de la URL');
     }
-    res.end()
+    res.end(`La letra que corresponde al DNI insertardo es la ${getLetterDNI(req.params.dni)} y su DNI completo es ${req.params.dni}${getLetterDNI(req.params.dni)}`);
+})
+
+app.get('/', (req, res) => {
+    res.redirect('/calcular-dni/')
 })
 
 app.listen(port, () => {
